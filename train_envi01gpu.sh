1bf97df9b20c582a8906cbd44fcccd97cc6d3ea8
Aditi Jain <67906321+AditiJain14@users.noreply.github.com>
2023-02-24 11:29:38 -0800

    Update train_envi01gpu.sh

--- train_envi01gpu.sh
+++ train_envi01gpu.sh
@@ -112,7 +112,7 @@ mma_il_with_pretrained(){
 mma_il_lm(){
     lambda=$1
     # name="single_path_latency_${lambda}"
-    name="lmloss_latency_0.1_0.3_${lambda}"
+    name="lmloss_latency_0.1_0.15_${lambda}"
     export WANDB_NAME="${name}"
 
     CKPT="${EXPT}/infinite/${name}/checkpoints"
@@ -152,7 +152,7 @@ mma_il_lm(){
     --add-language-model \
     --share-lm-decoder-softmax-embed \
     --pretrain-steps 3000 \
-    --token-scale 0.1 --sentence-scale 0.3 \
+    --token-scale 0.1 --sentence-scale 0.15 \
     --wandb-project LM_Adaptive_EnVi \
     --empty-cache-freq 45 --max-epoch 50\
     | tee -a ${TBOARD}/train_log.txt
