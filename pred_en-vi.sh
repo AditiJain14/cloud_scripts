3d1eca9d18db2b31296a220fbd56112c2a2913ec
Aditi Jain <67906321+AditiJain14@users.noreply.github.com>
2023-02-25 01:51:20 -0800

    Update pred_en-vi.sh

--- pred_en-vi.sh
+++ pred_en-vi.sh
@@ -41,8 +41,8 @@ generate_single_path(){
     # python generate.py ${DATA} --path $modelfile/average-model.pt --batch-size 1 --beam 1 --left-pad-source False --fp16  --remove-bpe --test-wait-k ${testk} --sim-decoding > pred.out
 
     # ckpt=average-model.pt
-    #ckpt="checkpoint_best.pt"
-    ckpt="avgmodel_50.pt"
+    ckpt="checkpoint50.pt"
+#     ckpt="avgmodel_50.pt"
 
     # batched prediction
     python "${FAIRSEQ}/fairseq_cli/generate.py" ${data} \
