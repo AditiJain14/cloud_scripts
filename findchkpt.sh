0f3e17d9e83d890be2ce8ee55cd76f0ab6a39704
Aditi Jain <67906321+AditiJain14@users.noreply.github.com>
2023-03-08 12:48:11 -0800

    Update findchkpt.sh

--- findchkpt.sh
+++ findchkpt.sh
@@ -60,7 +60,7 @@ generate_single_path(){
         python "${FAIRSEQ}/fairseq_cli/generate.py" ${data} \
         --path "${modelfile}/${ckpt}" \
         --source-lang "de" --target-lang "en" \
-        --batch-size 50 \
+        --batch-size 500 \
         --beam 1 \
         --left-pad-source \
         --remove-bpe \
