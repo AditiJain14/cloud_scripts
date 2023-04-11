fdc5d4d7f63d9d99a9cea826dd0b28bedf2262d9
Aditi Jain <67906321+AditiJain14@users.noreply.github.com>
2023-04-11 16:52:41 -0700

    Update pred_wmt.sh

--- pred_wmt.sh
+++ pred_wmt.sh
@@ -38,7 +38,7 @@ generate_single_path(){
     # bsz 1 prediction
     # python generate.py ${DATA} --path $modelfile/average-model.pt --batch-size 1 --beam 1 --left-pad-source False --fp16  --remove-bpe --test-wait-k ${testk} --sim-decoding > pred.out
 
-    ckpt=avgmodel_32.pt
+    ckpt=avgmodel_31.pt
 #     ckpt="checkpoint_best.pt"
 #       ckpt="average-model.pt"
     # ckpt="checkpoint40.pt"
