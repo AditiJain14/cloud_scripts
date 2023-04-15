376521f62c8c8e5450f7b06fe7f33800dc2d0b0a
Aditi Jain <67906321+AditiJain14@users.noreply.github.com>
2023-04-15 11:38:00 -0700

    Update pred_wmt.sh

--- pred_wmt.sh
+++ pred_wmt.sh
@@ -24,7 +24,7 @@ data="${DATA}"
 generate_single_path(){
     lambda=0.04 #$1
 
-    name="lmlossApril8_0.4" #"latency_${lambda}"
+    name="lmlossApril14_0.1_0.1_0.3" #"latency_${lambda}"
 
     EXPT="${EXP}/infinite/${name}"
     RES="${EXPT}/results/action"
